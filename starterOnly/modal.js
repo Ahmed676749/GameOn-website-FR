cefa175d95a54c39569d933e63337cd3a408e76f
Ahmed <ahmed676749@yahoo.com>
2024-05-30 14:53:23 +0200

    fermeture modal, vérification nom et prénom

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,66 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// récupérer la span close
+const btnClose = document.getElementsByClassName("close")[0]; // [0] = before et [1] = after
 
+// fermeture de la modal lors du clic sur le bouton span close
+btnClose.onclick = function() {
+  modalbg.style.display = "none";
+}
+
+// récupérer l'info du prénom
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+
+// fonction qui valide le prenom
+function isFirstNameValid() {
+  const isValid = first.value.length >= 2; 
+  if(!isValid) {
+    displayError(first);
+  } else {
+    hideError(first);
+  } 
+  return isValid;
+}
+
+// fonction qui valide le nom
+function isLastNameValid() {
+  const isValid = last.value.length >= 2; 
+  if(!isValid) {
+    displayError(last);
+  } else {
+    hideError(last);
+  } 
+  return isValid;
+}
+
+function isFormValid() {
+  return isFirstNameValid() && isLastNameValid();
+}
+
+// gestion de la soummission
+const btnSubmit = document.getElementsByClassName("btn-submit")[0];
+btnSubmit.addEventListener("click", function(event) {
+  event.preventDefault();
+  if(isFormValid()) {
+    alert('ok');
+  } else {
+    alert("no ok");
+  }
+});
+
+// gestion des erreurs
+function displayError(input) {
+  const formData = input.parentNode;
+  formData.setAttribute("data-error-visible", "true");
+  formData.setAttribute("data-error", "Le prénom est invalide");
+  formData.setAttribute("data-error", "Le nom est invalide");
+}
+
+// cacher les erreurs
+function hideError(input) {
+  const formData = input.parentNode;
+  formData.removeAttribute("data-error-visible");
+  formData.removeAttribute("data-error");
+}
