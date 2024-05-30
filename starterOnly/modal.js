90ff51cb604c9170b6a03e38809680eb398c8148
Ahmed <ahmed676749@yahoo.com>
2024-05-30 19:55:22 +0200

    vérification email et date de naissance

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,12 +31,15 @@ btnClose.onclick = function() {
 // récupérer l'info du prénom
 const first = document.getElementById("first");
 const last = document.getElementById("last");
+const email = document.getElementById("email");
+
+
 
 // fonction qui valide le prenom
 function isFirstNameValid() {
   const isValid = first.value.length >= 2; 
   if(!isValid) {
-    displayError(first);
+    displayError(first, "le prénom est invalide");
   } else {
     hideError(first);
   } 
@@ -45,17 +48,69 @@ function isFirstNameValid() {
 
 // fonction qui valide le nom
 function isLastNameValid() {
-  const isValid = last.value.length >= 2; 
-  if(!isValid) {
-    displayError(last);
+  const isValidLast = last.value.length >= 2; 
+  if(!isValidLast) {
+    displayError(last, "le nom est invalide");
   } else {
     hideError(last);
   } 
-  return isValid;
+  return isValidLast;
+}
+
+// fonction qui valide l'email
+function isEmailValid() {
+  const emailSchema = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailSchema.test(email.value)) {
+    displayError(email, "L'email est invalide");
+    return false;
+  } else {
+    hideError(email);
+    return true;
+  } 
+}
+
+
+// fonction qui valide la date
+function validateBirthdate(birthdate) {
+  if(!birthdate.value) {
+    displayError(birthdate, "La date de naissance est obligatoire");
+    return false;
+  }
+
+  const birthInput = new Date(birthdate.value);
+  const today = new Date();
+  const age = today.getFullYear() - birthInput.getFullYear();
+  const monthDifference = today.getMonth() - birthInput.getMonth();
+  const dayDifference = today.getDate() - birthInput.getDate();
+
+  // Vérifier si la date est dans le futur
+  if (birthInput > today) {
+      displayError(birthdate, "La date de naissance ne peut pas être dans le futur.");
+      return false;
+  }
+
+  // Vérifier si la date est valide
+  if (isNaN(birthInput.getTime())) {
+      displayError(birthdate, "La date de naissance est invalide.");
+      return false;
+  }
+
+  // Vérifier l'âge (par exemple, 18 ans)
+  if (age < 18 || (age === 18 && monthDifference < 0) || (age === 18 && monthDifference === 0 && dayDifference < 0)) {
+      displayError(birthdate, "Vous devez avoir au moins 18 ans.");
+      return false;
+  }
+
+  hideError(birthdate);
+  return true;
 }
 
 function isFormValid() {
-  return isFirstNameValid() && isLastNameValid();
+  const _isFirstValid = isFirstNameValid();
+  const _isLastNameValid = isLastNameValid();
+  const _isEmailValid = isEmailValid();
+  const _validateBirthdate = validateBirthdate(birthdate);
+  return _isFirstValid && _isLastNameValid && _isEmailValid && _validateBirthdate;
 }
 
 // gestion de la soummission
@@ -70,11 +125,10 @@ btnSubmit.addEventListener("click", function(event) {
 });
 
 // gestion des erreurs
-function displayError(input) {
+function displayError(input, errorMessage) {
   const formData = input.parentNode;
   formData.setAttribute("data-error-visible", "true");
-  formData.setAttribute("data-error", "Le prénom est invalide");
-  formData.setAttribute("data-error", "Le nom est invalide");
+  formData.setAttribute("data-error", errorMessage);
 }
 
 // cacher les erreurs
